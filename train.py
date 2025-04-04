1b458aa7b4a60c5805c17a433bcc0ec2508d1399
michaelmariaott <87768914+michaelmariaott@users.noreply.github.com>
2025-04-04 18:12:48 +0200

    added command line arguments to train.py

--- train.py
+++ train.py
@@ -55,33 +55,72 @@ def mlp_to_fluid_json_dict(mlp):
         json_dict["layers"][i]["weights"] = list([list(w) for w in weights[i]])
     return json_dict
 
-if __name__ == '__main__':
-
+def parse_arguments():
+  '''
+  parses commandline arguments, similar syntax to fluid.mlpregressor~
+  '''
   parser = argparse.ArgumentParser(
   prog='train',
   description='Train MLP from flucoma datasets'
   )
 
-  parser.add_argument('X')
-  parser.add_argument('y')
-  parser.add_argument('-o', '--output')
+  # inputs and outputs
+  parser.add_argument('X', help="Flucoma .json dataset file for the training inputs.")
+  parser.add_argument('y', help="Flucoma .json dataset file for the training ouputs.")
+  parser.add_argument('output', help=".json file to save the model")
 
-  args = parser.parse_args()
+  # Hidden layers
+  parser.add_argument('--hidden_layers', nargs='+', type=int, default=[3, 3], help="Number of neurons in hidden layers (default: [3, 3])")
+  
+  # Activation functions
+  parser.add_argument('--activation', type=str, choices=['identity', 'logistic', 'tanh', 'relu'], default='relu', help="Activation function for the hidden layers and output layer")
+  parser.add_argument('--output_activation', type=str, choices=['identity', 'logistic', 'tanh', 'relu'], default=None, help="Activation function for the output layer")
+  
+  # Learning rate
+  parser.add_argument('--learn_rate', type=float, default=0.01, help="Learning rate (default: 0.01)")
+  
+  # Maximum iterations
+  parser.add_argument('--max_iter', type=int, default=1000, help="Maximum number of iterations for training (default: 1000)")
+  
+  # Validation settings
+  parser.add_argument('--validation', action='store_true', default=False, help="Enable validation during training")
+  
+  # Batch size
+  parser.add_argument('--batch_size', type=int, default=50, help="Batch size for training (default: 50)")
+  
+  # Momentum
+  parser.add_argument('--momentum', type=float, default=0.9, help="Momentum factor for optimization (default: 0.9). Only used with sgd solver.")
+
+  # Solver
+  parser.add_argument('--solver', type=str, choices=['lbfgs', 'sgd', 'adam'], default='sgd', help="The solver for weight optimization (default: sgd).")
+  
+
+  return parser.parse_args()
+
+if __name__ == '__main__':
+
+  args = parse_arguments()
+  
 
   print('Reading ', args.X, args.y, 'from disc...')
   X = read_dataset_from_file(args.X)
   y = read_dataset_from_file(args.y)
   X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.1)
   mlp = MLPRegressor(
-    random_state=1,
-    max_iter=5000, 
-    activation='tanh', 
+    max_iter=args.max_iter, 
+    activation=args.activation, 
+    learning_rate_init=args.learn_rate,
+    early_stopping=args.validation,
+    batch_size=args.batch_size,
+    momentum=args.momentum,
     solver='adam',
     verbose=True
   )
+  if(args.output_activation):
+    mlp.out_activation_ = args.output_activation
   mlp.fit(X_train, y_train)
   fluid_json = mlp_to_fluid_json_dict(mlp)
-  write_model_to_file(fluid_json, f'{args.output}.json')
+  write_model_to_file(fluid_json, f'{args.output}')
 
   print('Score: ', mlp.score(X_test, y_test))
 
